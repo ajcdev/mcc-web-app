f4d2589033d9da7a6e9bb43961e4bc0911841c6c
Idii1114 <201812060@feualabang.edu.ph>
2022-05-30 08:44:52 +0800

    Project Screen

--- app/src/main/java/com/example/mccnewdesignapplication/MainActivity.java
+++ app/src/main/java/com/example/mccnewdesignapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.mccnewdesignapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
