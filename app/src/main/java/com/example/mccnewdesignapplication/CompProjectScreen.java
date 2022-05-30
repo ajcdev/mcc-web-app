7e8015caa19f9c2411a0eaea51fa6087324716d1
Idii1114 <201812060@feualabang.edu.ph>
2022-05-30 15:21:05 +0800

    Project Screens

---
+++ app/src/main/java/com/example/mccnewdesignapplication/CompProjectScreen.java
@@ -0,0 +1,14 @@
+package com.example.mccnewdesignapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompProjectScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comp_project_screen);
+    }
+}
---
+++ app/src/main/java/com/example/mccnewdesignapplication/OngProjectScreen.java
@@ -0,0 +1,14 @@
+package com.example.mccnewdesignapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OngProjectScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ong_project_screen);
+    }
+}
