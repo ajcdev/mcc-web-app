babff0488db73c3d6e90e22e5109f90a8fac1c89
Idii1114 <201812060@feualabang.edu.ph>
2022-05-31 16:04:32 +0800

    Project Screens

--- app/src/main/java/com/example/mccnewdesignapplication/MainActivity.java
+++ app/src/main/java/com/example/mccnewdesignapplication/MainActivity.java
@@ -16,20 +16,5 @@ public class MainActivity extends AppCompatActivity {
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         //getSupportActionBar().hide();
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Button btncomp = findViewById(R.id.compProj);
-        btncomp.setOnClickListener(v -> fragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainerView2, compproject.class, null)
-                .setReorderingAllowed(true)
-                .addToBackStack("name")
-                .commit());
-
-        Button btnong = findViewById(R.id.ongProj);
-        btnong.setOnClickListener(v -> fragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainerView2, ongproject.class, null)
-                .setReorderingAllowed(true)
-                .addToBackStack("name")
-                .commit());
-
     }
 }
