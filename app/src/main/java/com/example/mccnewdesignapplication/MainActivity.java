d32a7975206413a0ed418f254574d60e934dafa1
Idii1114 <201812060@feualabang.edu.ph>
2022-05-31 17:04:33 +0800

    Project Screens

--- app/src/main/java/com/example/mccnewdesignapplication/MainActivity.java
+++ app/src/main/java/com/example/mccnewdesignapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.mccnewdesignapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         //getSupportActionBar().hide();
+
+        CardView btnComp = findViewById(R.id.compProj);
+        CardView btnOng = findViewById(R.id.ongProj);
+
+        btnComp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, new compproject()).commit();
+            }
+        });
+
+        btnOng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, new ongproject()).commit();
+            }
+        });
 
     }
 }
