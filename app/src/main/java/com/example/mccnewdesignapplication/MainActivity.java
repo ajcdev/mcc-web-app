d97910f41fc4417d0e7ae600ab0acd5210fc4883
jiandaya <55966752+chirijidev@users.noreply.github.com>
2022-06-01 12:13:55 +0800

    test because it's crashing for some reason with no logcat

--- app/src/main/java/com/example/mccnewdesignapplication/MainActivity.java
+++ app/src/main/java/com/example/mccnewdesignapplication/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.mccnewdesignapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    NewsUpdatesFragment newsUpdatesFragment = new NewsUpdatesFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navbar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.newsupdates:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, newsUpdatesFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
