a54def187c3277ca469cbd21c6688365b820e5a3
Idii1114 <201812060@feualabang.edu.ph>
2022-06-01 11:15:07 +0800

    News and Updates Screen

--- app/src/main/java/com/example/mccnewdesignapplication/MainActivity.java
+++ app/src/main/java/com/example/mccnewdesignapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.mccnewdesignapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getSupportActionBar().hide();
+
+
     }
 }
--- app/src/main/java/com/example/mccnewdesignapplication/news_and_updates_screen.java
+++ app/src/main/java/com/example/mccnewdesignapplication/news_and_updates_screen.java
@@ -65,7 +65,7 @@ public class news_and_updates_screen extends Fragment {
         btnUpd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, updates_screen.class, null).commit();
+                getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, new news_and_updates_screen()).commit();
             }
         });
         return view;
