299f6c5f2443ffe3f4f7780222d4672dae827749
Idii1114 <201812060@feualabang.edu.ph>
2022-06-01 10:59:22 +0800

    News and Updates Screen

--- app/src/main/java/com/example/mccnewdesignapplication/news_and_updates_screen.java
+++ app/src/main/java/com/example/mccnewdesignapplication/news_and_updates_screen.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +60,14 @@ public class news_and_updates_screen extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_and_updates_screen, container, false);
+        View view= inflater.inflate(R.layout.fragment_news_and_updates_screen, container, false);
+        ImageButton btnUpd = (ImageButton) view.findViewById(R.id.updatesbtn);
+        btnUpd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, updates_screen.class, null).commit();
+            }
+        });
+        return view;
     }
 }
